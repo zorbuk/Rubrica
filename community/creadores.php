3337196f0247f5f3938bfa373a051ba8f7d07e12
Miquel Valero <45876289+zorbuk@users.noreply.github.com>
2019-05-17 13:27:58 +0200

    final

---
+++ community/creadores.php
@@ -0,0 +1,33 @@
+<?php
+require '../config/checkConnected.php';
+require '../config/db.php';
+require '../gParts/header.php';
+?> 
+
+<div class="my-3 p-3 bg-white rounded shadow-sm">
+        <h6 class="border-bottom border-gray pb-2 mb-0"><b>Los Creadores de Rubrica</b></h6>
+	<div class="card mb-3" style="max-width: 100%;">
+  <div class="row no-gutters">
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Miquel Valero Recasens</h5>
+        <p class="card-text"><small class="text-muted"><a href="mailto:noexiste@prueba.com"><img src="../images/mail.png"> Contactar</a></small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card mb-3" style="max-width: 100%;">
+  <div class="row no-gutters">
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Tudor Decebal Parlogea</h5>
+        <p class="card-text"><small class="text-muted"><a href="mailto:noexiste@prueba.com"><img src="../images/mail.png"> Contactar</a></small></p>
+      </div>
+    </div>
+  </div>
+</div>
+
+ <?php
+require '../gParts/footer.php';
+        ?>
